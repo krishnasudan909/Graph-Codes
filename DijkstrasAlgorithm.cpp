745eb0c71d6eec16d3b5f04f7497d165cb851424
Krishna <74672126+krishnasudan909@users.noreply.github.com>
2022-02-14 16:20:02 +0530

    Dijkstras Algorithm Updated

--- DijkstrasAlgorithm.cpp
+++ DijkstrasAlgorithm.cpp
@@ -1,28 +1,71 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Solution{
-vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
-    {
-        priority_queue<int,vector<int>,greater<int>> q;
-        vector<int> dis(V,INT_MAX);
-        dis[S]= 0;
-        q.push(S);
-        while(!q.empty()){
-            auto node = q.top();
-            q.pop();
-            int temp = dis[node];
-            for(auto& it: adj[node]){
-                if(temp+it[1]<dis[it[0]]){
-                    dis[it[0]] = temp + it[1];
-                    q.push(it[0]);
-                }
-            }
-        }
-        return dis;
-    }
-};
-int main()
-{
-    return 0;
+int main(){
+	int n,m,source;
+	cin >> n >> m;
+	vector<pair<int,int> > g[n+1]; 	// 1-indexed adjacency list for of graph
+
+	int a,b,wt;
+	for(int i = 0; i<m ; i++){
+		cin >> a >> b >> wt;
+		g[a].push_back(make_pair(b,wt));
+		g[b].push_back(make_pair(a,wt));
+	}	
+	
+	cin >> source;
+	
+	// Dijkstra's algorithm begins from here
+	priority_queue<pair<int,int>,vector<pair<int,int> >,greater<pair<int,int> > > pq;// min-heap ; In pair => (dist,from)
+	vector<int> distTo(n+1,INT_MAX); 	// 1-indexed array for calculating shortest paths; 
+	
+	distTo[source] = 0;
+	pq.push(make_pair(0,source));	// (dist,from)
+	
+	while( !pq.empty() ){
+		int dist = pq.top().first;
+		int prev = pq.top().second;
+		pq.pop();
+		
+		vector<pair<int,int> >::iterator it;
+		for( it = g[prev].begin() ; it != g[prev].end() ; it++){
+			int next = it->first;
+			int nextDist = it->second;
+			if( distTo[next] > distTo[prev] + nextDist){
+				distTo[next] = distTo[prev] + nextDist;
+				pq.push(make_pair(distTo[next], next));
+			}
+		}
+		
+	}
+	
+	cout << "The distances from source, " << source << ", are : \n";
+	for(int i = 1 ; i<=n ; i++)	cout << distTo[i] << " ";
+	cout << "\n";
+	
+	return 0;
 }
+
+
+
+/*
+Why Priority Queue?
+
+Striver why should we use priorty queue rather than queue, I implemented with queue also and got the same result
+
+take U forward
+ It will end up taking a lot more time, as you will not get the shorter distance at first instance, will have to do a lot of rounds.
+
+26
+
+try doing with this treee
+
+1 2 10
+2 3 15 
+1 4 1
+4 5 1 
+3 6 10
+5 6 1 
+
+In this queue, you will necessary travel some nodes, you reach the node 6 twice, which will be avoided in case of PQ, so in larger cases, this becomes more costlier..
+*/
