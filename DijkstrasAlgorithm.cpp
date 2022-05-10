7175e9fb81aeef2d6e573bb5ae4281ae297a0e82
Krishna <74672126+krishnasudan909@users.noreply.github.com>
2022-05-10 12:51:26 +0530

    Create DijkstrasAlgorithm.cpp

--- DijkstrasAlgorithm.cpp
+++ DijkstrasAlgorithm.cpp
@@ -4,36 +4,32 @@ using namespace std;
 int main(){
 	int n,m,source;
 	cin >> n >> m;
-	vector<pair<int,int> > g[n+1]; 	// 1-indexed adjacency list for of graph
+	vector<pair<int,int> > g[n+1];
 
 	int a,b,wt;
 	for(int i = 0; i<m ; i++){
 		cin >> a >> b >> wt;
-		g[a].push_back(make_pair(b,wt));
-		g[b].push_back(make_pair(a,wt));
+		g[a].push_back({b,wt});
+		g[b].push_back({a,wt});
 	}	
-	
 	cin >> source;
-	
-	// Dijkstra's algorithm begins from here
-	priority_queue<pair<int,int>,vector<pair<int,int> >,greater<pair<int,int> > > pq;// min-heap ; In pair => (dist,from)
-	vector<int> distTo(n+1,INT_MAX); 	// 1-indexed array for calculating shortest paths; 
+	priority_queue<pair<int,int>,vector<pair<int,int> >,greater<pair<int,int> > > pq;
+	vector<int> distTo(n+1,INT_MAX);
 	
 	distTo[source] = 0;
-	pq.push(make_pair(0,source));	// (dist,from)
+	pq.push({0,source});
 	
 	while( !pq.empty() ){
 		int dist = pq.top().first;
 		int prev = pq.top().second;
 		pq.pop();
-		
-		vector<pair<int,int> >::iterator it;
-		for( it = g[prev].begin() ; it != g[prev].end() ; it++){
-			int next = it->first;
-			int nextDist = it->second;
-			if( distTo[next] > distTo[prev] + nextDist){
-				distTo[next] = distTo[prev] + nextDist;
-				pq.push(make_pair(distTo[next], next));
+	
+		for( auto it: g[prev]){
+			int next = it.second;
+			int nextDist = it.first;
+			if( distTo[next] > dist + nextDist){
+				distTo[next] = dist + nextDist;
+				pq.push({distTo[next], next});
 			}
 		}
 		
@@ -47,25 +43,15 @@ int main(){
 }
 
 
-
 /*
 Why Priority Queue?
-
-Striver why should we use priorty queue rather than queue, I implemented with queue also and got the same result
-
-take U forward
  It will end up taking a lot more time, as you will not get the shorter distance at first instance, will have to do a lot of rounds.
-
-26
-
-try doing with this treee
-
+Example:
 1 2 10
 2 3 15 
 1 4 1
 4 5 1 
 3 6 10
 5 6 1 
-
 In this queue, you will necessary travel some nodes, you reach the node 6 twice, which will be avoided in case of PQ, so in larger cases, this becomes more costlier..
 */
